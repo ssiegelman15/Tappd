23538fcbf2cc94b5abc3f897e91fbae4dfcdb07f
Shlomo Siegelman <ssiegelman15@gmail.com>
2023-03-29 19:20:42 -0600

    commented out currently unused lines

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,49 +11,59 @@ const typeDefs = gql`
     favoriteBreweries: [brewerySchema]!
   }
 
-  type Movie {
-    movieId: Int
-    title: String
-    description: String
-    image: String
-    backdrop: String
-    trailer: String
+  type Beer {
+    beerId: Int
+    beerName: String
+    beerDescription: String
+    beerImage: String
+    beerStyle: String
+    beerAbv: Int
     createdAt: String
+    beerBrewery: [brewerySchema]!
   }
 
-  input inputMovie {
-    movieId: Int
-    title: String
-    description: String
-    image: String
-    backdrop: String
-    trailer: String
-    createdAt: String
+  type Brewery {
+    breweryId: Int
+    breweryName: String
+    breweryLogo: String
+    breweryDescription: String
+    breweryLocation: String
   }
+
+  # input inputMovie {
+  #   movieId: Int
+  #   title: String
+  #   description: String
+  #   image: String
+  #   backdrop: String
+  #   trailer: String
+  #   createdAt: String
+  # }
 
   type Auth {
     token: ID!
     user: User
   }
 
-  type Query {
-    users: [User]
-    user(username: String!): User
-    movies(username: String): [Movie]
-    movie(movieId: String!): Movie
-    me: User
-  }
+  #   type Query {
+  #     users: [User]
+  #     user(username: String!): User
+  #     movies(username: String): [Movie]
+  #     movie(movieId: String!): Movie
+  #     me: User
+  #   }
 
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    saveMovie(movie: inputMovie!): User
-    likeMovie(movie: inputMovie!): User
-    favoriteMovie(movie: inputMovie!): User
-    removeMovie(movieId: Int!): User
-    unlikeMovie(movieId: Int!): User
-    unfavoriteMovie(movieId: Int!): User
-  }
+  #   type Mutation {
+  #     addUser(username: String!, email: String!, password: String!): Auth
+  #     login(email: String!, password: String!): Auth
+  #     saveMovie(movie: inputMovie!): User
+  #     likeMovie(movie: inputMovie!): User
+  #     favoriteMovie(movie: inputMovie!): User
+  #     removeMovie(movieId: Int!): User
+  #     unlikeMovie(movieId: Int!): User
+  #     unfavoriteMovie(movieId: Int!): User
+  #   }
+  #
 `;
 
 module.exports = typeDefs;
